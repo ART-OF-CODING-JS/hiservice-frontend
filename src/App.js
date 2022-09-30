401df00a359a88316a5b0beac88171e46fef125b
yaseinburqan <mr.yasein@gmail.com>
2022-09-30 22:54:50 +0000

    add Styling for Sign in form and Register form and Fixing Register api

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ import ForgetPassword from "./component/Auth/forget-password/forget-password.com
 import ServicesConfirmation from "./component/Admin-components/ServicesConfirmation/ServicesConfirmation";
 import Reservation from "./component/Admin-components/reservation/Reservation";
 import ContactUs from "./component/Admin-components/contact/ContactUs";
-import Favorite from './component/Favorite/Favorite'
-import HomePage from './component/homePage/HomePage'
+import Favorite from "./component/Favorite/Favorite";
+import HomePage from "./component/homePage/HomePage";
 function App() {
   const { isSignin } = useSelector((state) => state.authSlice);
   console.log(isSignin);
@@ -40,7 +40,6 @@ function App() {
           <Header />
           <ToastContainer />
           <Routes>
-            
             <Route
               path="/users"
               element={
@@ -49,7 +48,7 @@ function App() {
                 </Access>
               }
             ></Route>
-  
+
             <Route path="/Services" element={<Services />} />
             <Route path="/Services/:id" element={<ServiceDetails />} />
             <Route path="/My-Services" element={<MyServices />} />
@@ -64,7 +63,7 @@ function App() {
             <Route path="/Contact-us" element={<Contact />} />
             <Route path="/Contact-Admin" element={<ContactUs />} />
             <Route path="/Reports" element={<h1>Mohamamd</h1>} />
-            <Route path="/Settings" element={<Setting/>} />
+            <Route path="/Settings" element={<Setting />} />
 
             <Route path="/Settings" element={<Setting />} />
             <Route path="/search" element={<SearchService />} />
@@ -84,7 +83,7 @@ function App() {
       ) : (
         <>
           <Routes>
-            <Route path="/" element={<HomePage/>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/forgetpassword" element={<ForgetPassword />} />
           </Routes>
--- src/component/Auth/Signin/Signin.js
+++ src/component/Auth/Signin/Signin.js
@@ -1,11 +1,21 @@
 import React, { useState, useRef } from "react";
-import { MDBContainer, MDBTabs, MDBTabsItem, MDBTabsLink, MDBTabsContent, MDBTabsPane, MDBBtn, MDBInput, MDBCard, MDBCol, MDBRow, MDBCardImage, MDBCardBody, MDBIcon } from "mdb-react-ui-kit";
-import { useSelector, useDispatch } from "react-redux";
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBCard,
+} from "mdb-react-ui-kit";
+import { useDispatch } from "react-redux";
 import { signin } from "../../../store/auth";
 import Signup from "../Signup/Signup";
 import "./Signin.css";
+import Logo from "../../../assets/logo.png";
+import Footer from "../../footer/footer";
+
 export default function Signin() {
-  // const {isSignin} = useSelector((state)=>state.authSlice)
   const [justifyActive, setJustifyActive] = useState("tab1");
   const dispatch = useDispatch();
   const handleJustifyClick = (value) => {
@@ -31,60 +41,86 @@ export default function Signin() {
   };
   return (
     <>
-      {/* {errorSingin &&(<Alert severity="error">The username or password is incorrect</Alert>)} */}
-      {
-        //    !isSingin?
-        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-          <MDBTabs pills justify className="mb-3 d-flex flex-row justify-content-between">
-            <MDBTabsItem>
-              <MDBTabsLink className="login-switch" onClick={() => handleJustifyClick("tab1")} active={justifyActive === "tab1"}>
-                Login
-              </MDBTabsLink>
-            </MDBTabsItem>
-            <MDBTabsItem>
-              <MDBTabsLink className="register-switch" onClick={() => handleJustifyClick("tab2")} active={justifyActive === "tab2"}>
-                Register
-              </MDBTabsLink>
-            </MDBTabsItem>
-          </MDBTabs>
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <MDBTabs pills justify className="mb-3 d-flex flex-row justify-content-between">
+          <MDBTabsItem>
+            <MDBTabsLink
+              className="login-switch"
+              onClick={() => handleJustifyClick("tab1")}
+              active={justifyActive === "tab1"}
+            >
+              LogIn
+            </MDBTabsLink>
+          </MDBTabsItem>
+          <MDBTabsItem>
+            <MDBTabsLink
+              className="register-switch"
+              onClick={() => handleJustifyClick("tab2")}
+              active={justifyActive === "tab2"}
+            >
+              Register
+            </MDBTabsLink>
+          </MDBTabsItem>
+        </MDBTabs>
 
-          <MDBTabsContent>
-            <MDBTabsPane show={justifyActive === "tab1"}>
-              <MDBContainer className="my-5">
-                <MDBCard>
-                  <MDBRow className="g-0">
-                    <MDBCol md="6">
-                      <MDBCardImage src="https://i.ibb.co/k9BGqKk/spacejoy-x0-Nw1-Gvi6m-Q-unsplash-1-1.jpg" alt="login form" className="rounded-start w-100" />
-                    </MDBCol>
+        <MDBTabsContent>
+          <MDBTabsPane show={justifyActive === "tab1"}>
+            <MDBContainer className="container py-5 h-100">
+              <MDBCard style={{ borderRadius: "1rem" }}>
+                <div className="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
+                  <div className="card-body p-5 text-center">
+                    <div className="mb-md-5 mt-md-4 pb-5">
+                      <img src={Logo} width="175px" alt="" />
 
-                    <MDBCol md="6">
-                      <MDBCardBody className="d-flex flex-column">
-                        <div className="d-flex flex-row mt-2">
-                          <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: "#ff6219" }} />
-                          <span className="h1 fw-bold mb-0">HI SERVICE</span>
-                        </div>
+                      <h2 className="fw-bold mb-2 text-uppercase">LogIn</h2>
 
-                        <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: "1px" }}>
-                          Sign into your account
-                        </h5>
+                      <p className="text-white-50 mb-5">Please enter your Username and password!</p>
 
-                        <MDBInput wrapperClass="mb-4" label="Username" id="formControlLg" type="email" size="lg" inputRef={usernameRef} />
-                        <MDBInput wrapperClass="mb-4" label="Password" id="formControlLg" type="password" size="lg" inputRef={passwordRef} />
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="email"
+                          id="typeEmailX"
+                          className="form-control form-control-lg"
+                          ref={usernameRef}
+                        />
+                        <label className="form-label">Username</label>
+                      </div>
 
-                        <MDBBtn className="mb-4 px-5 com-btn" color="dark" size="lg" onClick={handleSignin}>
-                          Login
-                        </MDBBtn>
-                        <a className="small text-muted" href="/forgetpassword">
-                          Forget Password?
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="password"
+                          id="typePasswordX"
+                          className="form-control form-control-lg"
+                          ref={passwordRef}
+                        />
+                        <label className="form-label">Password</label>
+                      </div>
+
+                      <button
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="submit"
+                        onClick={handleSignin}
+                      >
+                        LogIn
+                      </button>
+
+                      <p className="small mb-5 pb-lg-2">
+                        <a className="text-white-50" href="/forgetpassword">
+                          Forgot password?
                         </a>
+                      </p>
 
-                        <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
-                          Don't have an account?{" "}
-                          <a href="#!" style={{ color: "#393f81" }} onClick={() => handleJustifyClick("tab2")} active={justifyActive === "tab2"}>
+                      <div>
+                        <p className="mb-0">
+                          Don't have an account?
+                          <a
+                            href="#!"
+                            className="text-white-50 fw-bold"
+                            onClick={() => handleJustifyClick("tab2")}
+                          >
                             Register here
                           </a>
                         </p>
-
                         <div className="d-flex flex-row justify-content-start">
                           <a href="#!" className="small text-muted me-1">
                             Terms of use.
@@ -93,19 +129,21 @@ export default function Signin() {
                             Privacy policy
                           </a>
                         </div>
-                      </MDBCardBody>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBCard>
-              </MDBContainer>
-            </MDBTabsPane>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </MDBCard>
+            </MDBContainer>
+          </MDBTabsPane>
 
-            <MDBTabsPane show={justifyActive === "tab2"}>
-              <Signup />
-            </MDBTabsPane>
-          </MDBTabsContent>
-        </MDBContainer>
-      }
+          <MDBTabsPane show={justifyActive === "tab2"}>
+            <Signup />
+          </MDBTabsPane>
+        </MDBTabsContent>
+      </MDBContainer>
+
+      <Footer />
     </>
   );
 }
