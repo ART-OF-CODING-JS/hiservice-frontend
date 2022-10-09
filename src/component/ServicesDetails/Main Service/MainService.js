6d5328a03eff372592a2be8df09b5888d83607cc
yaseinburqan <mr.yasein@gmail.com>
2022-10-09 17:05:48 +0000

    ff

--- src/component/ServicesDetails/Main Service/MainService.js
+++ src/component/ServicesDetails/Main Service/MainService.js
@@ -22,38 +22,37 @@ export default function MainService(props) {
               </div>
               <div className="info-div">
                 <nav className="main-service-nav">
-
                   <a href="#/">Created at : {ele.createdAt.substring(0, 10)}</a>
-                 {ele.userID=== parseInt(cookie.load('userID'))?<p style={{color:'red'}
-                }>Your Service</p>:null}
+                  {ele.userID === parseInt(cookie.load("userID")) ? (
+                    <p style={{ color: "red" }}>Your Service</p>
+                  ) : null}
                 </nav>
                 <h1 className="main-service-h1">
                   <span className="main-service-span">
                     <ServiceProviderInfo ServiceProviderId={ele.userID} />
                   </span>
-                  <h5>
-                    {ele.title}, in {ele.city}
-                  </h5>
+                  {ele.title}, in {ele.city}
                 </h1>
                 <p className="text-break">{ele.description}</p>
-                {console.log(!(ele.userID=== parseInt(cookie.load('userID'))),'dddddddd')}
-          {!(ele.userID=== parseInt(cookie.load('userID')))?<Access role={"user"}>
-                <div className="block-report">
-                    <div className="block-report-btn">
-                      <BlockProvider providerID={ele.userID} />
-                      <Reports providerID={ele.id} />
+                {console.log(!(ele.userID === parseInt(cookie.load("userID"))), "dddddddd")}
+                {!(ele.userID === parseInt(cookie.load("userID"))) ? (
+                  <Access role={"user"}>
+                    <div className="block-report">
+                      <div className="block-report-btn">
+                        <BlockProvider providerID={ele.userID} />
+                        <Reports providerID={ele.id} />
+                      </div>
+                      <Reservation serviceId={ele.id} />
+                      <Link
+                        className="btn btn-warning add-btn chat-button"
+                        style={{ fontSize: "15px", marginTop: "1px" }}
+                        to={`/chat/${ele.userID}`}
+                      >
+                        Chat <i class="fa fa-comments-o" aria-hidden="true"></i>
+                      </Link>
                     </div>
-                    <Reservation serviceId={ele.id} />
-                    <Link
-                      className="btn btn-warning add-btn chat-button"
-                      to={`/chat/${ele.userID}`}
-                    >
-                      Chat
-                    </Link>
                   </Access>
-
-                </div>
-                  </Access>:null}
+                ) : null}
               </div>
             </div>
           </div>
