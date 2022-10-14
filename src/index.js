4a549ec6dda5a2d542222bd2e9e7e89866158580
Muradazzeh <morad.azza@yahoo.com>
2022-10-15 01:37:18 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ import "./component/homePage/assets/css/agency.min.css";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
+  
 
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
-  // </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
